117dce25189eda2c11da74343af8c7fb5e1b40d8
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-01 03:14:09 +0600

    display details info added

--- js/app.js
+++ js/app.js
@@ -28,21 +28,24 @@ const getInputData = () => {
     return inputValue;
 }
 
-const processSearchResult = (data) => {
-    const phones = data;
+const processSearchResult = (phones) => {
+    console.log(phones);
     clearContainer();
     if (phones.length) {
 
         //pass only 20 result
         displaySearchResult(phones.slice(0, 20));
 
+
+       if(phones.length>20){
         const showMoreBtn = document.getElementById('show-more-btn');
         showMoreBtn.classList.remove('d-none');
-
         showMoreBtn.addEventListener('click', () => {
             showMoreBtn.classList.add('d-none');
+            console.log(phones)
             displaySearchResult(phones.slice(20, phones.length));
         });
+       }
 
 
 
@@ -72,7 +75,7 @@ const displaySearchResult = (phones) => {
                 </div>
                 </div>
                 <div>
-                <button class="btn btn-color mb-3 w-25">Detail</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-color mb-3 w-25">Details</button>
                 </div>
               </div>
         `;
@@ -81,8 +84,55 @@ const displaySearchResult = (phones) => {
     });
 }
 
+const loadPhoneDetails = async (phoneId) =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    try {
+        const res = await fetch(url);
+        const resData = await res.json();
+        console.log(resData.data);
+        displayPhoneDetails(resData.data);
+    } catch (e) {
+
+    }
+}
+
+const displayPhoneDetails = (phoneInfo) =>{
+    const phoneDetailContainer = document.getElementById('phone-detain-container');
+    phoneDetailContainer.parentNode.classList.add('card')
+    const {chipSet,displaySize,memory,sensors} = phoneInfo.mainFeatures;
+    console.log(chipSet,displaySize,memory,sensors)
+    phoneDetailContainer.innerHTML=`
+    <div class="col-md-4">
+    <div class="text-center d-flex align-items-center m-md-5">
+    
+    <img src="${phoneInfo.image}" class="mx-auto  img-fluid rounded-start" alt="phone Info image">
+    </div>
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">${phoneInfo.name}</h5>
+                      
+                      <p class="card-text"><span class="fw-bold">Release Date :</span> <br>${phoneInfo.releaseDate?phoneInfo.releaseDate:'No release date found'}</p>
+                      <p><span class="fw-bold">Brand : </span><br>${phoneInfo.brand}</p>
+                      
+                      
+                      <p class="card-text"><span class="fw-bold">chipSet :</span> <br>${chipSet}</p>
+                      
+                      <p class="card-text"><span class="fw-bold">Display Size :</span> <br>${displaySize}</p>
+                      
+                      <p class="card-text"><span class="fw-bold">Memory :</span> <br>${memory}</p>
+                      
+                      <p class="card-text"><span class="fw-bold">Sensors :</span> <br>${sensors.join(', ')}</p>
+                      <p class="card-text"></p>
+                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                  </div>
+    `
+}
+
 const clearContainer = () => {
     document.getElementById('search-result-container').textContent = '';
+    document.getElementById('show-more-btn').classList.add('d-none');
     // document.getElementById('show-more-btn').textContent ='';
 
 };
