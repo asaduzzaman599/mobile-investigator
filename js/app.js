55add1af595f0dbd3a87204e4b62f4e31f05ac37
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-28 22:56:07 +0600

    search phone data fetched

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('connected')
+// on clicked button load mobile data 
+const loadMobilesData = async () =>{
+    //get input data
+    const searchText = getInputData();
+
+    if(searchText){
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+
+        }catch(e){
+
+        }
+        console.log(inputValue)
+
+    }else{
+        console.log("empty")
+
+    }
+}
+
+const getInputData= () =>{
+    const inputField = document.getElementById("search-input");
+    const inputValue = inputField.value;
+    
+    //clearinput field
+    inputField.value = '';
+
+    return inputValue;
+}
